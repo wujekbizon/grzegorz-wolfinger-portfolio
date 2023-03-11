b18ae0c8aaf32ac86996792928b679ee7b1c1126
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-03-11 20:17:28 +0100

    create TypingText component

---
+++ components/animation/CustomText.tsx
@@ -0,0 +1,24 @@
+import styles from './CustomText.module.scss'
+import { ReactNode } from 'react'
+import { motion } from 'framer-motion'
+import { textContainer, textVariant } from '../../utils/motion'
+
+type TypingTextProps = {
+  title: string
+  textStyles?: string
+}
+
+type TextProps = {
+  title: ReactNode
+  textStyles?: string
+}
+
+export const TypingText = ({ title, textStyles }: TypingTextProps) => (
+  <motion.p variants={textContainer} className={`${textStyles} ${styles.text_typed}`}>
+    {Array.from(title).map((letter, index) => (
+      <motion.span variants={textVariant} key={index}>
+        {letter === ' ' ? '\u00A0' : letter}
+      </motion.span>
+    ))}
+  </motion.p>
+)
