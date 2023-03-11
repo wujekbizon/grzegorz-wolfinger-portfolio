be7bae411aa58158bd0daf2ac829cc1e416a616b
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-03-11 18:52:02 +0100

    start work on Hero section

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,6 +1,18 @@
 import styles from './Hero.module.scss'
+import Image from 'next/image'
 
 const Hero = () => {
-  return <section>Hero</section>
+  return (
+    <section className={`${'section_wrapper'} ${styles.hero}`}>
+      <div className={styles.left}>
+        <h1>Todays web reach it's limits</h1>
+        <h4></h4>
+      </div>
+
+      <div className={styles.right}>
+        <Image src="/images/cloud.png" alt="cloud" width={520} height={300} />
+      </div>
+    </section>
+  )
 }
 export default Hero
