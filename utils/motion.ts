02e5f75923e2ec711371f52ce5b48052ae3ca75d
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-03-11 19:19:00 +0100

    adding framer-motion and animation functions

---
+++ utils/motion.ts
@@ -0,0 +1,183 @@
+import { Variants } from 'framer-motion';
+
+export const navVariants = {
+  hidden: {
+    opacity: 0,
+    y: -50,
+    transition: {
+      type: 'spring',
+      stiffness: 300,
+      damping: 140,
+    },
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 80,
+      delay: 0.4,
+    },
+  },
+};
+
+export const staggerContainer = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.5,
+      delayChildren: 0.5,
+    },
+  },
+} as Variants;
+
+export const textTitleVariant = (delay: number) => ({
+  hidden: {
+    y: 50,
+    opacity: 0,
+  },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      duration: 1.25,
+      delay,
+    },
+  },
+});
+
+export const textContainer = {
+  hidden: {
+    opacity: 0,
+  },
+  show: (i = 1) => ({
+    opacity: 1,
+    transition: { staggerChildren: 0.04, delayChildren: i * 0.04 },
+  }),
+};
+
+export const titleTextContainer = {
+  hidden: {
+    opacity: 0,
+  },
+  show: (i = 1) => ({
+    opacity: 1,
+    transition: { staggerChildren: 0.1, delayChildren: i * 0.1 },
+  }),
+};
+
+export const textVariant = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'tween',
+      ease: 'easeIn',
+    },
+  },
+};
+
+export const fadeIn = (
+  direction: string,
+  type: string,
+  delay: number,
+  duration: number
+) => ({
+  hidden: {
+    x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0,
+    y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
+    opacity: 0,
+  },
+  show: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      type,
+      delay,
+      duration,
+      ease: 'easeOut',
+    },
+  },
+});
+
+export const zoomIn = (delay: number, duration: number) => ({
+  hidden: {
+    scale: 0,
+    opacity: 0,
+  },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: 'tween',
+      delay,
+      duration,
+      ease: 'easeOut',
+    },
+  },
+});
+
+export const slideIn = (
+  direction: string,
+  type: string,
+  delay: number,
+  duration: number
+) => ({
+  hidden: {
+    x: direction === 'left' ? '-150%' : direction === 'right' ? '100%' : 0,
+    y: direction === 'up' ? '-100%' : direction === 'down' ? '100%' : 0,
+  },
+  show: {
+    x: 0,
+    y: 0,
+    transition: {
+      type,
+      delay,
+      duration,
+      ease: 'easeOut',
+    },
+  },
+});
+
+export const titleVariants = (direction: string, rotate = 0) => ({
+  hidden: {
+    x: direction === 'left' ? '-100%' : '100%',
+    rotate: 120,
+  },
+  show: {
+    x: 0,
+    rotate: rotate,
+    transition: {
+      type: 'spring',
+      duration: 1.8,
+      delay: 0.5,
+    },
+  },
+});
+
+export const footerVariants = {
+  hidden: {
+    opacity: 0,
+    y: 50,
+    transition: {
+      type: 'spring',
+      stiffness: 300,
+      damping: 140,
+    },
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 80,
+      delay: 0.5,
+    },
+  },
+};
