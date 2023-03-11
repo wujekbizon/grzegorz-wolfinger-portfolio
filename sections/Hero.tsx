9c119c8e57678efe5f92e3ce17053dd1449d654d
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-03-11 20:11:30 +0100

    setup ityped

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,11 +1,32 @@
 import styles from './Hero.module.scss'
 import Image from 'next/image'
+import { useState, useEffect, useRef } from 'react'
+import { init } from 'ityped'
+
+const text = ["Todays web, slowly reaching it's limits.", 'Clouds services are getting more expensive.']
 
 const Hero = () => {
+  const textRef = useRef<HTMLHeadingElement>(null)
+  const [debouncedText, setDebouncedText] = useState(text)
+
+  useEffect(() => {
+    if (!textRef.current) {
+      return
+    }
+
+    init(textRef.current, {
+      cursorChar: '',
+      showCursor: true,
+      strings: debouncedText,
+      backDelay: 1800,
+      backSpeed: 20
+    })
+  }, [debouncedText])
+
   return (
     <section className={`${'section_wrapper'} ${styles.hero}`}>
       <div className={styles.left}>
-        <h1>Todays web reach it's limits</h1>
+        <h1 className={styles.ityped} ref={textRef}></h1>
         <h4></h4>
       </div>
 
