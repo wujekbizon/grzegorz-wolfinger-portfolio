2ec275a87792acbd84c13c8bb119749db423ff83
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-03-26 12:19:32 +0200

    add dots animation

---
+++ data/customers.ts
@@ -0,0 +1,28 @@
+const generateId = () => {
+  return Math.random()
+}
+// Temp Array with customers
+
+export const customers = [
+  { id: generateId(), name: 'Ichigo Kurosaki', top: '16%', left: '25%' },
+  { id: generateId(), name: 'Rukia Kuchiki', top: '27%', left: '39%' },
+  { id: generateId(), name: 'Orihime Inoue', top: '20%', left: '15%' },
+  { id: generateId(), name: 'Renji Abarai', top: '12%', left: '50%' },
+  { id: generateId(), name: 'Uryū Ishida', top: '18%', left: '60%' },
+  { id: generateId(), name: 'Yasutora Sado', top: '65%', left: '20%' },
+  { id: generateId(), name: 'Sōsuke Aizen', top: '9%', left: '45%' },
+  { id: generateId(), name: 'Byakuya Kuchiki', top: '35%', left: '10%' },
+  { id: generateId(), name: 'Kisuke Urahara', top: '30%', left: '50%' },
+  { id: generateId(), name: 'Yoruichi Shihōin', top: '50%', left: '4%' },
+  { id: generateId(), name: 'Suì-Feng', top: '3%', left: '35%' },
+  { id: generateId(), name: 'Rangiku Matsumoto', top: '23%', left: '45%' },
+  { id: generateId(), name: 'Retsu Unohana', top: '30%', left: '70%' },
+  { id: generateId(), name: 'Mayuri Kurotsuchi', top: '30%', left: '86%' },
+  { id: generateId(), name: 'Kenpachi Zaraki', top: '50%', left: '40%' },
+  { id: generateId(), name: 'Sajin Komamura', top: '10%', left: '20%' },
+  { id: generateId(), name: 'Izuru Kira', top: '20%', left: '80%' },
+  { id: generateId(), name: 'Isshin Kurosaki', top: '30%', left: '18%' },
+  { id: generateId(), name: 'Shunsui Kyoraku', top: '70%', left: '47%' },
+  { id: generateId(), name: 'Jushiro Ukitake', top: '80%', left: '85%' },
+  { id: generateId(), name: 'Toshiro Hitsugaya', top: '12%', left: '8%' }
+]
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion'
 import { staggerContainer, fadeIn, zoomIn } from '@/utils/motion'
 import { TypingText } from '@/components/animation/CustomText'
 import Image from 'next/image'
+import { customers } from '@/data/customers'
 
 const Hero = () => {
   return (
@@ -10,16 +11,35 @@ const Hero = () => {
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: true, amount: 0.25 }}
+      viewport={{ once: false, amount: 0.25 }}
       className={styles.hero}
     >
       <div className={styles.hero_wrapper}>
-        <motion.div variants={zoomIn(1, 2)} className={styles.title_container}>
-          <h1>Today's web is pushing the boundaries of what is possible.</h1>
-          <Image src="/images/map.png" alt="map" width={700} height={350} className={styles.map} />
+        <motion.div variants={fadeIn('left', 'tween', 0.3, 1)} className={styles.title_container}>
+          <motion.h1 className={`${styles.hero_uppertitle}`} variants={fadeIn('down', 'tween', 2, 2)}>
+            Today's web is pushing the boundaries of what is possible.
+          </motion.h1>
+          <div className={styles.map_container}>
+            {customers.map((customer, index) => (
+              <motion.div
+                variants={zoomIn(index * 0.5, 0.4)}
+                key={customer.id}
+                className={styles.dot}
+                style={{ top: customer.top, left: customer.left }}
+              >
+                <div className={styles.dot_container}>
+                  <p className={styles.dot_content}>{customer.name}</p>
+                </div>
+              </motion.div>
+            ))}
+            <Image src="/images/map.png" alt="map" width={700} height={350} className={styles.map} />
+          </div>
+          <motion.h1 className={` ${styles.hero_lowertitle}`} variants={fadeIn('up', 'tween', 2.5, 2)}>
+            Let's create <span>Together</span>, network of endless possibilities.
+          </motion.h1>
         </motion.div>
       </div>
-      <motion.h6 className={styles.scroll} variants={fadeIn('up', 'tween', 2, 1)}>
+      <motion.h6 className={styles.scroll} variants={fadeIn('up', 'tween', 3, 1)}>
         <span>&#8650; </span>Scroll down
         <div className="footer_gradient" />
       </motion.h6>
