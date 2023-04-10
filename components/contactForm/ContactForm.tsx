f16b0545c1379e1b1952be8ea47de84a785786e2
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-04-10 14:06:26 +0200

    add ContactForm component

--- components/contactForm/ContactForm.tsx
+++ components/contactForm/ContactForm.tsx
@@ -1,9 +1,80 @@
 import styles from './ContactForn.module.scss'
+import React, { ChangeEventHandler, FormEventHandler, useRef, useState } from 'react'
+import { motion } from 'framer-motion'
+import { slideIn } from '../../utils/motion'
 
 const ContactForm = () => {
+  const formRef = useRef<HTMLFormElement>(null)
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const [loading, setLoading] = useState(false)
+
+  const handleChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = (e) => {
+    const { name, value } = e.target
+
+    setForm({
+      ...form,
+      [name]: value
+    })
+  }
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
+    setLoading(true)
+  }
   return (
-    <div className={styles.form}>
-      <h1>ContactForm</h1>
+    <div className={styles.contact}>
+      <motion.div variants={slideIn('left', 'tween', 0.2, 1)} className={styles.form_container}>
+        <p className={styles.subtitle}>Get in touch</p>
+        <h2 className={styles.title}>Contact.</h2>
+
+        <form ref={formRef} onSubmit={handleSubmit} className={styles.form}>
+          <label className={styles.label}>
+            <span className={styles.span}>Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
+            <span className={styles.span}>Your email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your web address?"
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
+            <span className={styles.span}>Your Message</span>
+            <textarea
+              rows={7}
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className={styles.input}
+            />
+          </label>
+
+          <button type="submit" className={styles.btn}>
+            {loading ? 'Sending...' : 'Send'}
+          </button>
+        </form>
+      </motion.div>
+      <motion.div variants={slideIn('right', 'tween', 0.2, 1)} className={styles.canvas_container}>
+        <h1>Hello</h1>
+      </motion.div>
     </div>
   )
 }
--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,15 +1,17 @@
 import styles from './Footer.module.scss'
 import { Social, ContactForm } from '@/components'
 import { SectionWrapper } from '@/hoc'
+import { motion } from 'framer-motion'
+import { text2Variant } from '../utils/motion'
 
 const Footer = () => {
   return (
     <>
       <div className={styles.footer}>
-        <Social />
         <ContactForm />
+        <Social />
       </div>
     </>
   )
 }
-export default SectionWrapper(Footer, 'footer')
+export default SectionWrapper(Footer, 'contact')
