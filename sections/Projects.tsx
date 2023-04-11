90a1ce559a76aca02a984c4d125f57368f338ad3
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-04-11 20:34:27 +0200

    add more content to Projects

--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -7,34 +7,47 @@ import { fadeIn, staggerContainer } from '@/utils/motion'
 
 const Projects = ({ projects }: ProjectsProps) => {
   return (
-    <section className={styles.projects}>
+    <motion.section
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={styles.projects}
+    >
       <ProjectsHero />
-      <motion.div
-        variants={staggerContainer}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: false, amount: 0.25 }}
-      >
-        <motion.div id="projects" variants={fadeIn('left', 'tween', 1, 1)} className={styles.feature}>
-          <p className={styles.subtitle}>Featured Live Demo Project</p>
-          <h2 className={styles.title}>
-            Realistic 3D Environment with <span className={styles.span}>Three.js</span>{' '}
-          </h2>
-          <motion.p variants={fadeIn('left', 'tween', 1.2, 1)} className={styles.content}>
-            {' '}
-            This project was a great learning experience for me. I learned how to create a realistic 3D environment with
-            Three.js, setting up the scene, adding textures and materials, creating lighting, and adding objects. I also
-            learned how to use the Three.js library to create a realistic 3D environment with various features such as
-            shadows, reflections, and more. Overall, this project was an excellent way for me to gain experience in
-            creating 3D environments with Three.js.
-          </motion.p>
-        </motion.div>
+
+      <motion.div id="projects" variants={fadeIn('left', 'tween', 1, 1)} className={styles.feature}>
+        <p className={styles.subtitle}>Featured Live Demo Project</p>
+        <h2 className={styles.title}>
+          Realistic 3D Environment with <span className={styles.span}>Three.js</span>{' '}
+        </h2>
+        <motion.p variants={fadeIn('left', 'tween', 1.2, 1)} className={styles.content}>
+          {' '}
+          This project was a great learning experience for me. I learned how to create a realistic 3D environment with
+          Three.js, setting up the scene, adding textures and materials, creating lighting, and adding objects. I also
+          learned how to use the Three.js library to create a realistic 3D environment with various features such as
+          shadows, reflections, and more. Overall, this project was an excellent way for me to gain experience in
+          creating 3D environments with Three.js.
+        </motion.p>
       </motion.div>
+
       <div className={styles.three_canvas_container}>
         <ThreeCanvas />
       </div>
+      <motion.div id="projects" variants={fadeIn('right', 'tween', 1, 1)} className={styles.project}>
+        <p className={styles.subtitle}>Real World Projects</p>
+        <h2 className={styles.title}>
+          Developing <span className={styles.span}>Innovative</span> Solutions
+        </h2>
+        <motion.p variants={fadeIn('right', 'tween', 1.2, 1)} className={styles.content}>
+          My portfolio webpage showcases the real-world projects I have completed, demonstrating my technical skills and
+          abilities as a developer. I am constantly learning new technologies and techniques to stay up-to-date with the
+          latest industry trends. I am now focusing on changing the web as we know it today by building more complex
+          cross-platform applications and connecting them in a P2P network.
+        </motion.p>
+      </motion.div>
       <ProjectList projects={projects} />
-    </section>
+    </motion.section>
   )
 }
 export default Projects
