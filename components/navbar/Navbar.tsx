a946773aff8fa3d0189920e6106d1bf619d2adac
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-04-13 17:37:17 +0200

    add navbar menu hamburger

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -58,16 +58,15 @@ const Navbar = () => {
             onClick={() => setToggle(!toggle)}
           />
 
-          <div
-            className={`${
-              !toggle ? 'hidden' : 'flex'
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-          >
-            <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
+          <div className={!toggle ? styles.hidden : styles.show}>
+            <ul className={styles.menu_list}>
               {links.map(({ label, url }) => (
                 <li
                   className={url === router.pathname ? `${styles.active} ${styles.link_btn}` : `${styles.link_btn}`}
                   key={label}
+                  onClick={() => {
+                    setToggle(!toggle)
+                  }}
                 >
                   <Link href={url}>{label}</Link>
                 </li>
