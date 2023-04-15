08b24fe237816de9687818ffd2ed5c39a0dd97cc
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-04-15 14:36:41 +0200

    setup toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,16 @@ import '@/styles/globals.scss'
 import '@/styles/gradients.scss'
 import '@/styles/fonts.scss'
 import '@/styles/canvas.scss'
+import 'react-toastify/dist/ReactToastify.css'
 import type { AppProps } from 'next/app'
 import { Layout } from '@/components'
+import { ToastContainer, Zoom } from 'react-toastify'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <ToastContainer position="top-center" transition={Zoom} draggablePercent={60} />
     </Layout>
   )
 }
